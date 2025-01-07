afc02a1ec8c43b125abb004634425f96972e817c
abijith <abijithumax100@gmail.com>
2025-01-07 17:39:38 -0500

    add reset pass API

--- src/components/ResetPass/ResetPass.js
+++ src/components/ResetPass/ResetPass.js
@@ -1,43 +1,49 @@
-import React, { useEffect, useState } from 'react'
-import { Alert, Button } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Button } from 'react-bootstrap'
 import Axios from '../../config/axios'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import './ResetPass.css'
+import { toast } from 'react-toastify'
 
 
 function ResetPass() {
+    const navigate = useNavigate()
     const { token } = useParams()
-    const [trueuser, setTrueuser] = useState(false)
-    const [message,setMessage] = useState('Token Error')
+    const [password, setPassword] = useState()
+    const [cpassword, setCpassword] = useState()
 
-    useEffect(() => {
+    function ResetAPI() {
+        if (password !== cpassword) {
+            toast.warn("Check Confirm Password")
+            return;
+        }
         Axios({
-            url: 'auth/verify-token',
-            method: 'POST',
-            data: { token }
+            url:'auth/reset-password',
+            method:'POST',
+            data: { password,token }
         })
-            .then(response => {
-                if (response.data.status) {
-                    setTrueuser(true)
-                }else{
-                    setTrueuser(false)
-                    setMessage(response.data.message)
-                }
-            })
-            .catch(err => console.log(err))
-    }, [token])
+        .then(response => {
+            if(response.data.status){
+                toast.success(response.data.message)
+                navigate('/signin')
+            }else{
+                toast.warn(response.data.message)
+            }
+        })
+    }
+
     return (
         <div className='reset-pass'>
             <title>Reset Password</title>
             <h1>Reset Password</h1>
-            {trueuser ?
-                <div className='form'>
-                    <input type="password" placeholder='Password' />
-                    <input type="password" placeholder='Confirm Password' />
-                    <div>
-                        <Button>Save</Button>
-                    </div>
-                </div> : <Alert>{message}</Alert>}
+
+            <div className='form'>
+                <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                <input type="password" placeholder='Confirm Password' value={cpassword} onChange={(e) => setCpassword(e.target.value)} />
+                <div>
+                    <Button onClick={ResetAPI}>Save</Button>
+                </div>
+            </div>
 
         </div>
     )
